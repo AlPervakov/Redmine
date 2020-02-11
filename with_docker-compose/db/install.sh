128969ef7d7fa745d7be9c8ef1cb364d34f4b4b8
AlPervakov <official.creperum@gmail.com>
2020-02-11 14:08:02 +0300

    add install script db

---
+++ with_docker-compose/db/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt update
+apt install -y mysql-server
+
+echo '[mysqld]' > /etc/mysql/mysql.cnf
+echo 'bind-address = 0.0.0.0' > /etc/mysql/mysql.cnf
+
+service mysql start
+
+mysql -u root --execute="CREATE DATABASE redmine character SET utf8;"
+mysql -u root --execute="CREATE user 'redmine'@'%' IDENTIFIED BY 'redmine';"
+mysql -u root --execute="GRANT ALL privileges ON redmine.* TO 'alex'@'%';"
